b92d363c03d03e4d4f805e0d7eb25816c3cce072
daniil <trostin1998@bk.ru>
2022-06-05 21:13:39 +0300

    first

---
+++ src/main/java/ru/test/skyprofirst/SkyprofirstApplication.java
@@ -0,0 +1,13 @@
+package ru.test.skyprofirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyprofirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyprofirstApplication.class, args);
+	}
+
+}
